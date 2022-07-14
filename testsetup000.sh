c9acdb9a3f5841f7fef0a9f13485ee69173ffd34
YetAnotherGeorge <48997150+YetAnotherGeorge@users.noreply.github.com>
2022-07-14 15:21:07 +0300

    Add files via upload

--- testsetup000.sh
+++ testsetup000.sh
@@ -11,8 +11,17 @@
 #          [keyfile]
 #          unmanaged-devices=*,except:type:ethernet,except:type:gsm,except:type:cdma
 #    # disable wifi:
-#    echo "iface wlan0 inet manual" >> /etc/network/interfaces
+#    manual/etc/network/interfaces iface wlan0 inet 
+#       iface wlan0 inet 
 #    systemctl restart NetworkManager
+#    #Disable Wait For Online 
+#      systemctl disable systemd-networkd-wait-online.service
+#      systemctl mask systemd-networkd-wait-online.service
+#    #Ignore Laptop Lid Closing
+#    sudo gedit /etc/systemd/logind.conf 
+#       HandleLidSwitch=ignore
+#       LidSwitchIgnoreInhibited=no
+TIMECTL
    
 
 # Must be ran as root using:
@@ -24,41 +33,40 @@
 #        IHRC.BashConfig.001.sh        #bash
 #        IHRC.SSH.001.conf             #SSH
 #        IHRC.Nginx-RTMP.001.conf      #Nginx-RTMP (using port 1935)
-#        IHRC.smb.001.conf             #Samba
+#        IHRC.smb.003.conf             #Samba
 #        IHRC.qbittorrent.001.service  #qbittorent
 #        
 #        testsetup000.sh
 #
 #     ======================================================================================
-#     /home
-#        /ihrcdata      -> drwxr-xr-x  4  ihrcdata ihrcdata
-#           /backup     -> drwxrws---  2  ihrc-bak ihrc-bak
-#           /media      -> drwxrws---  2  ihrc-med ihrc-med
-#              /Seagate -> drwxrws---  2  ihrc-med ihrc-med
+#     /ihrcdata      -> drwxr-xr-x  4  root     root
+#        /backup     -> drwxrws---  2  IHRCsmb  IHRCsmb
+#        /media      -> drwxrws---  2  IHRCsmb  IHRCsmb
+#           /Seagate -> drwxrws---  2  IHRCsmb  IHRCsmb
 #     ======================================================================================
 #     qbittorent:
 #        App Data:   /home/qbittorrent-user
-#        Downloads:  /home/ihrcdata/media/Seagate/seedbox
+#        Downloads:  /ihrcdata/media/Seagate/seedbox
 #     ======================================================================================
 #     jellyfin:
 #        App Data:   /home/jellfin-user
-#        Media:      /home/ihrcdata/media/
+#        Media:      /ihrcdata/media/
 #     ======================================================================================
 #=====================[Install Config]=====================
 #       SSH Port          :    
 # user  root     password : 
 # user  NONROOT  password : 
-# user  ihrc-bak password : 
-# user  ihrc-med password : 
-# !qbittorrent-nox      : save path must be /home/ihrcdata/media/Seagate/seedbox
+# user  IHRCsmb password  : IHRCsmb
+# !qbittorrent-nox        : save path must be /ihrcdata/media/Seagate/seedbox
 #==========================================================
-
 
 
 set -e # exit when any command fails
 IHRC_DIR=$PWD #save github pull dir in case it's ever changed
+timedatectl set-timezone Europe/Bucharest
 
 # Perform updates & install all required packages for the rest of the process
+   apt purge samba* #in case old versions cause bugs although they probably can't 
    apt update && apt upgrade -y && mandb
 
    apt install -y              \
@@ -116,6 +124,42 @@ ufw enable
 
    systemctl enable ssh --now
 #
+# Create media server file structure + users + group assignments + fstab
+   useradd --system --shell /usr/sbin/nologin IHRCsmb
+   printf "\nEnter IHRCsmb user's password: \n"
+   passwd IHRCsmb
+   printf "\nEnter IHRCsmb user's samba password (same as user password): \n"
+   smbpasswd -a IHRCsmb
+
+   mkdir --mode=750 /ihrcdata && cd /ihrcdata
+   mkdir backup && mkdir media && mkdir media/Seagate 
+   chmod -R 2750 * && chown -R IHRCsmb:IHRCsmb *
+
+   printf "\n#LABEL=Seagate             /ihrcdata/media/Seagate     ext4     defaults          0        2\n" >> /etc/fstab
+   printf "Manually Configure Seagate (must have label 'Sagate' in fstab, must be mounted on /ihrcdata/media/Seagate), then continue script execution...\n"
+   read
+#
+# Samba
+   cd $IHRC_DIR
+   cp IHRC.smb.003.conf /etc/samba/smb.conf 
+   testparm -s
+   systemctl restart smbd
+   ufw allow samba
+#
+# Configure qbittorrent (UI on port 8085)
+   useradd --system --shell /usr/sbin/nologin --create-home qbittorrent-user
+   usermod -G IHRCsmb qbittorrent-user
+
+   mkdir --mode=2770 /ihrcdata/media/Seagate/seedbox
+   chown qbittorrent-user:IHRCsmb /ihrcdata/media/Seagate/seedbox
+
+   cd $IHRC_DIR
+   cp IHRC.qbittorrent.001.service /etc/systemd/system/qbittorrent.service
+   systemctl daemon-reload 
+   systemctl enable qbittorrent
+   systemctl start qbittorrent
+   ufw allow 8085 comment "qbittorrent web UI"
+#
 # Configure NGINX-Rtmp on port 1935
    cd $IHRC_DIR
    systemctl disable nginx --now
@@ -126,52 +170,9 @@ ufw enable
 
    systemctl enable nginx --now
 #
-# Create media server file structure + users + group assignments + fstab
-   useradd --system --shell /usr/sbin/nologin ihrc-bak
-   useradd --system --shell /usr/sbin/nologin ihrc-med
-
-   cd /home
-   mkdir --mode=755 ihrcdata && cd ihrcdata
-
-   mkdir --mode=2770 backup && chown ihrc-bak:ihrc-bak backup #2770 => d rwx rws --- 
-   mkdir --mode=2770 media  && chown ihrc-med:ihrc-med media  #2770 => d rwx rws --- 
-   mkdir --mode=2770 media/Seagate && chown ihrc-med:ihrc-med media/Seagate
-
-   #set passwords for backup and media:
-   printf "\nEnter ihrc-bak (backup) user's password: \n"
-   passwd ihrc-bak
-   printf "\nEnter ihrc-med (media)  user's password: \n"
-   passwd ihrc-med
-   printf "\n"
-
-   printf "\n#LABEL=Seagate             /home/ihrcdata/media/Seagate     ext4     defaults          0        2\n" >> /etc/fstab
-   printf "Manually Configure Seagate (must have label 'Sagate' in fstab, must be mounted on /home/ihrcdata/media/Seagate), then continue script execution...\n"
-   read TMP_VARIABLE
-#
-# Samba
-   cd $IHRC_DIR
-   cp IHRC.smb.001.conf /etc/samba/smb.conf 
-   testparm
-   systemctl restart smbd
-   ufw allow samba
-#
-# Configure qbittorrent (UI on port 8085)
-   useradd --system --shell /usr/sbin/nologin --create-home qbittorrent-user
-   usermod -G ihrc-med qbittorrent-user
-
-   mkdir --mode=2770 /home/ihrcdata/media/Seagate/seedbox
-   chown qbittorrent-user:ihrc-med /home/ihrcdata/media/Seagate/seedbox
-
-   cd $IHRC_DIR
-   cp IHRC.qbittorrent.001.service /etc/systemd/system/qbittorrent.service
-   systemctl daemon-reload 
-   systemctl enable qbittorrent
-   systemctl start qbittorrent
-   ufw allow 8085 comment "qbittorrent web UI"
-#
 # Jellyfin Docker 
    useradd --system --shell /usr/sbin/nologin --create-home jellyfin-user
-   usermod -G ihrc-med jellyfin-user
+   usermod -G IHRCsmb jellyfin-user
 
    mkdir --mode=750 /home/jellyfin-user/config /home/jellyfin-user/cache
    chown jellyfin-user:jellyfin-user /home/jellyfin-user/config /home/jellyfin-user/cache
@@ -185,7 +186,7 @@ ufw enable
       --net=host                                   \
       --volume /home/jellyfin-user/config:/config  \
       --volume /home/jellyfin-user/cache:/cache    \
-      --mount type=bind,source=/home/ihrcdata/media,target=/media \
+      --mount type=bind,source=/ihrcdata/media,target=/media \
       --restart=unless-stopped                     \
       jellyfin/jellyfin
 
@@ -210,3 +211,4 @@ ufw enable
    ufw status  >> testsetup000.statuslog.log
 
    vim testsetup000.statuslog.log
+#
